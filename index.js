c2b5f7fb8a67b4dbe0533fef5a1a32f7bae6a875
Jafet19 <kacur99@gmail.com>
2022-08-29 12:16:47 -0500

    commit tree 49ba2bc112652418fe16df9329eca15879f0b718

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2  
+const random = Math.ceil(Math.random())
+let num3 = 2
+let num4 = 1.5
+const mod = num3 + Math.ceil(num4)
+const max = 10 * 2
